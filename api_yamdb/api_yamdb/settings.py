e9963e2fbe351437e646f861e1ab9a9766d4f6ce
nikita-git <nikita.maximov@gmail.com>
2024-10-16 16:44:02 +0300

    Создал модель пользователя с пользовательскими ролями.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from datetime import timedelta
+import os
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -101,3 +102,7 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = 'users.User'
+
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, 'sent_emails')
+EMAIL_HOST_USER = 'nikita.maximov@gmail.com'
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    ADMIN = 'admin'
+    USER = 'user'
+    MODERATOR = 'moderator'
+
+    USER_ROLES = [
+        (ADMIN, 'Admin role'),
+        (USER, 'User role'),
+        (MODERATOR, 'Moderator role')
+    ]
+
+    email = models.EmailField(max_length=128, unique=True)
+    role = models.CharField(
+        max_length=16,
+        choices=USER_ROLES,
+        default='user'
+    )
+    token = models.CharField(
+        max_length=256,
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        ordering = ('username')
+
+    def is_admin(self):
+        return self.role == self.ADMIN or self.is_superuser
+
+    def is_moderator(self):
+        return self.role == self.MODERATOR
+
+    def __str__(self):
+        return self.username
