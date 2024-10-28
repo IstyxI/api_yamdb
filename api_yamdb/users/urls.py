c17e837d2963607b41646bdb76dc4e5966754ec6
nikita-git <nikita.maximov@gmail.com>
2024-10-28 13:52:16 +0300

    Перенес вьюхи users в api.

--- api_yamdb/users/urls.py
+++ api_yamdb/users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from users.views import (
+from api.views import (
     UserCreateViewSet,
     UserReceiveTokenViewSet,
     UserViewSet
