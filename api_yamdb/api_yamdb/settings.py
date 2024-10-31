0f82c138684025e07feed989c198075759898421
xDELETEDx <andrewow2030@gmail.com>
2024-10-31 20:19:06 +0300

    123

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -18,7 +18,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_simplejwt',
     'django_filters',
     'reviews.apps.ReviewsConfig',
     'users.apps.UsersConfig',
